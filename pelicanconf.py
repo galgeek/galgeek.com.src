fceff9b4af8d9248a48ffc12b6e6084c2cfbc03b
galgeek <galgeek@galgeek.com>
2014-09-24 07:53:27 -0700

    config!

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,14 +21,29 @@ TRANSLATION_FEED_ATOM = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         )
 
 # Social widget
-SOCIAL = (('twitter', 'http://twitter.com/galgeek'),)
+SOCIAL = (('github','http://github.com/galgeek'),
+          ('linkedin','www.linkedin.com/in/baramiller/'),
+          ('twitter', 'http://twitter.com/galgeek'),
+          ('phone','tel:1-503-616-5427'),)
 
 DEFAULT_PAGINATION = 10
 
 THEME = 'pelican-bootstrap3'
 BOOTSTRAP_THEME = 'cerulean'
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+DISPLAY_CATEGORIES_ON_MENU = False
+OUTPUT_RETENTION = ('.git', 'CNAME')
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+DATE_FORMATS = {
+        'en': '%A, %d %B %Y',
+}
+TWITTER_USERNAME = 'galgeek'
+GITHUB_URL = 'http://github.com/galgeek'
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
